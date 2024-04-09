b9d4462810bbe28b19394e948772e3d1e3a423ae
Lee0701 <hgl1002@naver.com>
2024-04-09 20:16:27 +0900

    Improve screenshot with mode selection and auto naming

--- examples/screenshot.py
+++ examples/screenshot.py
@@ -6,7 +6,7 @@ import logging
 import sys
 import datetime
 
-async def main(hostname, port, password):
+async def main(hostname, port, password, type):
     logger = logging.getLogger(__name__ + '.main')
     client = openttd.admin.Client()
     client.on_error = logger.error
@@ -26,15 +26,26 @@ async def main(hostname, port, password):
         return
 
     logger.info("Connected to server: %s", client.server_info.name)
-    
-    now = datetime.datetime.now()
-    now = now.strftime("%Y-%m-%d %H:%M:%S")
 
-    await client.rcon_command(f'screenshot minimap "map {now}"')
-    await client.rcon_command(f'screenshot topography "topo {now}"')
+    sys_date = await client.rcon_command(f'getsysdate')
+    sys_date = [d for _, d in sys_date][0]
+    game_date = await client.rcon_command(f'getdate')
+    game_date = [d for _, d in game_date][0]
+
+    sys_date, game_date = [d.split(':')[1].strip() for d in [sys_date, game_date]]
+    
+    # print(f'system date: {sys_date}, game date: {game_date}')
+
+    allowed_types = ['minimap', 'topography', 'industry']
+    if type not in allowed_types:
+        print(f'invalid type: {type}')
+        return
+
+    filename = f'screenshot_{type}_{sys_date}_{game_date}'
+    await client.rcon_command(f'screenshot {type} "{filename}"')
 
     await client.disconnect()
 
 if __name__ == "__main__":
-    [hostname, port, password] = sys.argv[1:]
-    asyncio.run(main(hostname, int(port), password))
+    [hostname, port, password, type] = sys.argv[1:]
+    asyncio.run(main(hostname, int(port), password, type))
